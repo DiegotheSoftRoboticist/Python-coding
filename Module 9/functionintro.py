619ba68ab718fd17a23d6add75efda6d717afbe2
Diego Higueras-Ruiz <83054728+DiegotheSoftRoboticist@users.noreply.github.com>
2021-06-23 10:06:34 -0700

    Add files via upload

---
+++ Module 9/functionintro.py
@@ -0,0 +1,8 @@
+def sing_happy_brithday(name):
+	print("Happy birthday to you")
+	print("Happy birthday to you")
+	print(f"Happy birthday dear {name}")
+	print("Happy birthday to you")
+
+sing_happy_brithday("Diego")
+
