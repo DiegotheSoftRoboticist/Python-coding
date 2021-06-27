c27bdc67c432a39ebb95151bd5ca1acb061b208e
Diego Higueras-Ruiz <83054728+DiegotheSoftRoboticist@users.noreply.github.com>
2021-06-27 11:08:22 -0700

    Add files via upload

---
+++ Module 10/functionlastelement.py
@@ -0,0 +1,11 @@
+def last_element(lista):
+    if lista:
+        return lista[len(lista)]
+    return None
+
+print(last_element([1,2,3,4,5,6]))
+
+# def last_element(l):
+#     if l:
+#         return l[-1]
+#     return None
